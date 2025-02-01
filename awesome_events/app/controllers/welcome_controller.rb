b5ca93ea5b2ec3bc69e57177a3089676d3b827ce
Shigeki Saito <shigeki.saito@bitstar.tokyo>
2025-02-01 11:26:23 +0900

    Revert "add: elasticsearchで検索機能を追加"
    This reverts commit ea7ef146ef48a2bb5ab964f8424b695577b36601.

--- awesome_events/app/controllers/welcome_controller.rb
+++ awesome_events/app/controllers/welcome_controller.rb
@@ -5,16 +5,7 @@ class WelcomeController < ApplicationController
     # kaminariを追加することでpage, perメソッドが使えるようになる
     # pageメソッドでページ番号を指定し、perメソッドで1ページあたりの表示件数を指定
     # perを指定しない場合はデフォルトの25件が表示される
-    # @events = Event.page(params[:page]).per(10).
-    #   where("start_at >  ?", Time.zone.now).order(:start_at)
-
-    @event_search_form = EventSearchForm.new(event_search_form_params)
-    @events = @event_search_form.search
-  end
-
-  private
-
-  def event_search_form_params
-    params.fetch(:event_search_form, {}).permit(:keyword, :start_at).merge(page: params[:page])
+    @events = Event.page(params[:page]).per(10).
+      where("start_at >  ?", Time.zone.now).order(:start_at)
   end
 end
